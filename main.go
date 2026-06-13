31e53e40928257fdfd94e5ac524489e6099e50f0
Perchinka <aleksei.v.lukin@gmail.com>
2026-06-13 22:19:26 +0100

    Update main to use service again

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	tea "charm.land/bubbletea/v2"
+	"perchinka.github.io/spelling-gopher/internal/infra/zenquotes"
+	"perchinka.github.io/spelling-gopher/internal/quote"
 	"perchinka.github.io/spelling-gopher/internal/tui"
 )
 
 func main() {
-	// client := &http.Client{Timeout: 5 * time.Second}
-	// repo := zenquotes.New(client)
-	// service := quote.NewService(repo)
-	model := tui.New()
+	client := &http.Client{Timeout: 5 * time.Second}
+	repo := zenquotes.New(client)
+	service := quote.NewService(repo)
+
+	model := tui.New(service)
 
 	if _, err := tea.NewProgram(model).Run(); err != nil {
 	}
