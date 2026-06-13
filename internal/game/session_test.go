e6292f25c4c1ffa47419825bbb3eb5b73ca27050
Perchinka <aleksei.v.lukin@gmail.com>
2026-06-13 10:24:46 +0100

    Add boundaries and current rune tests

--- internal/game/session_test.go
+++ internal/game/session_test.go
@@ -62,3 +62,31 @@ func TestSession_Glyphs_States(t *testing.T) {
 		})
 	}
 }
+
+func TestSession_Glyphs_CurrentRune(t *testing.T) {
+	s := NewSession("cat")
+	typeString(&s, "cx")
+	g := s.Glyphs()
+
+	if g[1].current != 'x' {
+		t.Errorf("wrong glyph current = %q, want 'x'", g[1].current)
+	}
+	if g[2].current != 't' {
+		t.Errorf("pending glyph current = %q, want 't'", g[2].current)
+	}
+}
+
+func TestSession_Glyphs_Boundaries(t *testing.T) {
+	s := NewSession("ab")
+	typeString(&s, "abc")
+
+	glyphs := s.Glyphs()
+	if len(glyphs) != 2 {
+		t.Fatalf("Glyphs() length = %d, want 2", len(glyphs))
+	}
+
+	last := glyphs[len(glyphs)-1]
+	if last.state != Correct {
+		t.Errorf("last glyph state = %v, want Correct", last.state)
+	}
+}
