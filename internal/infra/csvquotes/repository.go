b9d7cbed0675396e4ef6904c9c9e2a4d249bfa7f
Perchinka <aleksei.v.lukin@gmail.com>
2026-06-14 12:01:52 +0100

    Add local quotes repo

---
+++ internal/infra/csvquotes/repository.go
@@ -0,0 +1,42 @@
+package csvquotes
+
+import (
+	"context"
+	_ "embed"
+	"encoding/csv"
+	"math/rand/v2"
+	"strings"
+
+	"perchinka.github.io/spelling-gopher/internal/quote"
+)
+
+var _ quote.Repository = (*Repository)(nil)
+
+//go:embed quotes.csv
+var quotesData string
+
+type Repository struct {
+	quotes []quote.Quote
+}
+
+func New() (*Repository, error) {
+	records, err := csv.NewReader(strings.NewReader(quotesData)).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	quotes := make([]quote.Quote, 0, len(records))
+	for _, row := range records[1:] {
+		quotes = append(quotes, quote.Quote{
+			Text:   row[0],
+			Author: row[1],
+		})
+	}
+	return &Repository{
+		quotes: quotes,
+	}, nil
+}
+
+func (r *Repository) Random(ctx context.Context) (quote.Quote, error) {
+	return r.quotes[rand.IntN(len(r.quotes))], nil
+}
--- main.go
+++ main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"time"
 
 	tea "charm.land/bubbletea/v2"
-	"perchinka.github.io/spelling-gopher/internal/infra/zenquotes"
+	"perchinka.github.io/spelling-gopher/internal/infra/csvquotes"
 	"perchinka.github.io/spelling-gopher/internal/quote"
 	"perchinka.github.io/spelling-gopher/internal/tui"
 )
 
 func main() {
-	client := &http.Client{Timeout: 15 * time.Second}
-	repo := zenquotes.New(client)
+	// client := &http.Client{Timeout: 15 * time.Second}
+	// repo := zenquotes.New(client)
+	repo, err := csvquotes.New()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	service := quote.NewService(repo)
 
 	model := tui.New(service)
