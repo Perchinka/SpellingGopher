266cb5809c3f454ecec27c386c30f8248d231cbe
Perchinka <aleksei.v.lukin@gmail.com>
2026-06-15 18:56:21 +0100

    Fix clock to use fakeEpoch

--- internal/game/helpers_test.go
+++ internal/game/helpers_test.go
@@ -2,10 +2,12 @@ package game
 
 import "time"
 
-type fakeClock struct{ t time.Time }
+var fakeEpoch = time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC)
 
-func (c *fakeClock) Now() time.Time          { return c.t }
-func (c *fakeClock) Advance(d time.Duration) { c.t = c.t.Add(d) }
+type fakeClock struct{ elapsed time.Duration }
+
+func (c *fakeClock) Now() time.Time          { return fakeEpoch.Add(c.elapsed) }
+func (c *fakeClock) Advance(d time.Duration) { c.elapsed += d }
 
 func newTestSession(target string) *Session {
 	return NewSession(target, &fakeClock{})
